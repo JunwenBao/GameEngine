1f3625b04eaa97801fd0feec9d96ff4a2d1b6f6d
Wayents <JunwenBao@users.noreply.github.com>
2025-05-07 13:34:04 +0800

    Add premake

--- GameEngine/src/GameEngine/Log.cpp
+++ GameEngine/src/GameEngine/Log.cpp
@@ -10,7 +10,7 @@ namespace GameEngine
 	{
 		spdlog::set_pattern("%^[%T] %n: %v%$");
 
-		s_CoreLogger = spdlog::stdout_color_mt("HAZEL");
+		s_CoreLogger = spdlog::stdout_color_mt("ENGINE");
 		s_CoreLogger->set_level(spdlog::level::trace);
 
 		s_ClientLogger = spdlog::stdout_color_mt("APP");
