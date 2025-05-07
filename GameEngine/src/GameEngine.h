350e6a5cdf46e4e5cdb94ded0a4430652cc3e440
Wayents <JunwenBao@users.noreply.github.com>
2025-05-07 10:51:57 +0800

    Add logging system

--- GameEngine/src/GameEngine.h
+++ GameEngine/src/GameEngine.h
@@ -2,6 +2,7 @@
 
 //For use by application
 #include"GameEngine/Application.h"
+#include"GameEngine/Log.h"
 
 // --Entry Point--------------------------------
 #include"GameEngine/EntryPoint.h"
--- GameEngine/src/GameEngine/EntryPoint.h
+++ GameEngine/src/GameEngine/EntryPoint.h
@@ -6,6 +6,11 @@ extern GameEngine::Application* GameEngine::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	GameEngine::Log::Init();
+	HZ_CORE_WARN("Initialized Log");
+	int a = 5;
+	HZ_INFO("Hello! Var={0}", a);
+
 	auto app = GameEngine::CreateApplication();
 	app->Run();
 	delete app;
