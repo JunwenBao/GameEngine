ec2f6d5a19891cfa5bb690bafdeda8f5a51e87fc
Wayents <JunwenBao@users.noreply.github.com>
2025-05-26 13:52:52 +0800

    Use subtexture draw a map

--- SandBox/src/Sandbox2D.cpp
+++ SandBox/src/Sandbox2D.cpp
@@ -4,6 +4,15 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+static const uint32_t s_MapWidth = 20;
+static const char* s_MapTiles = "WWWWWWWWWWWWWWWWWWWW"
+								"WWWWWWWWWWWWWWWWWWWW"
+								"WWWWWWWWGGGGGWWWWWWW"
+								"WWWWWWWWGGWGGWWWWWWW"
+								"WWWWWWWWGGGGGWWWWWWW"
+								"WWWWWWWWWWWWWWWWWWWW"
+								"WWWWWWWWWWWWWWWWWWWW";
+
 Sandbox2D::Sandbox2D() : Layer("Sandbox2D"), m_CameraController(1280.0f / 720.0f), m_SquareColor({ 0.2f, 0.3f, 0.8f, 1.0f })
 {
 }
@@ -13,8 +22,11 @@ void Sandbox2D::OnAttach()
 	m_CheckerboardTexture = GameEngine::Texture2D::Create("assets/textures/Checkerboard.png");
 	m_SpriteSheet = GameEngine::Texture2D::Create("assets/game/textures/SpriteSheet.png");
 
-	m_TextureGrass = GameEngine::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 2, 5 }, { 16, 16 });
-	m_TextureWater = GameEngine::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 6, 6 }, { 16, 16 });
+	s_TextureMap['G'] = GameEngine::SubTexture2D::CreateFromCoords(m_SpriteSheet, {2, 5}, {16, 16});
+	s_TextureMap['W'] = GameEngine::SubTexture2D::CreateFromCoords(m_SpriteSheet, { 6, 6 }, { 16, 16 });
+
+	m_MapWidth = s_MapWidth;
+	m_MapHeight = strlen(s_MapTiles) / s_MapWidth;
 
 	// Init Particle System
 	m_Particle.ColorBegin = { 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
@@ -24,6 +36,8 @@ void Sandbox2D::OnAttach()
 	m_Particle.Velocity = { 0.0f, 0.0f };
 	m_Particle.VelocityVariation = { 3.0f, 1.0f };
 	m_Particle.Position = { 0.0f, 0.0f };
+
+	m_CameraController.SetZoomLevel(5.0f);
 }
 
 void Sandbox2D::OnDetach()
@@ -65,6 +79,7 @@ void Sandbox2D::OnUpdate(GameEngine::TimeStep ts)
 	//GameEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
 	//GameEngine::Renderer2D::EndScene();
+	*/
 
 	// œ‘ æParticle System
 	if (GameEngine::Input::IsMouseButtonPressed(HZ_MOUSE_BUTTON_LEFT))
@@ -85,10 +100,29 @@ void Sandbox2D::OnUpdate(GameEngine::TimeStep ts)
 	}
 	m_ParticleSystem.OnUpdate(ts);
 	m_ParticleSystem.OnRender(m_CameraController.GetCamera());
-	*/
+	
 	GameEngine::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	GameEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_TextureGrass);
-	GameEngine::Renderer2D::DrawQuad({ 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_TextureWater);
+	for (uint32_t y = 0; y < m_MapHeight; y++)
+	{
+		for (uint32_t x = 0; x < m_MapWidth; x++)
+		{
+			char tileType = s_MapTiles[x + y * m_MapWidth];
+			GameEngine::Ref<GameEngine::SubTexture2D> texture;
+
+			if (s_TextureMap.find(tileType) != s_TextureMap.end())
+			{
+				texture = s_TextureMap[tileType];
+			}
+			else
+			{
+				texture = m_TextureWater;
+			}
+
+			GameEngine::Renderer2D::DrawQuad({ x - m_MapWidth / 2.0f, m_MapHeight - y - m_MapHeight / 2.0f, 0.5f }, { 1.0f, 1.0f }, texture);
+		}
+	}
+	//GameEngine::Renderer2D::DrawQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_TextureGrass);
+	//GameEngine::Renderer2D::DrawQuad({ 1.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, m_TextureWater);
 	GameEngine::Renderer2D::EndScene();
 }
 
--- SandBox/src/Sandbox2D.h
+++ SandBox/src/Sandbox2D.h
@@ -31,4 +31,7 @@ private:
 
 	ParticleSystem m_ParticleSystem;
 	ParticleProps m_Particle;
+
+	uint32_t m_MapWidth, m_MapHeight;
+	std::unordered_map<char, GameEngine::Ref<GameEngine::SubTexture2D>> s_TextureMap;
 };
