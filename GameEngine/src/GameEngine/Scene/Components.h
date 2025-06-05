8d72cf59582ea81de845acdf39915bfe32f6a07d
Wayents <JunwenBao@users.noreply.github.com>
2025-06-05 20:18:19 +0800

    Add texture drag/drop and tiling factor to Sprite Component

--- GameEngine/src/GameEngine/Scene/Components.h
+++ GameEngine/src/GameEngine/Scene/Components.h
@@ -8,6 +8,7 @@
 
 #include "SceneCamera.h"
 #include "ScriptableEntity.h"
+#include "GameEngine/Renderer/Texture.h"
 
 namespace GameEngine {
 
@@ -46,9 +47,12 @@ namespace GameEngine {
 		}
 	};
 
+	/* ×é¼þ£ºSprite Renderer */
 	struct SpriteRendererComponent
 	{
 		glm::vec4 Color{ 1.0f, 1.0f, 1.0f, 1.0f };
+		Ref<Texture2D> Texture;
+		float TilingFactor = 1.0f;
 
 		SpriteRendererComponent() = default;
 		SpriteRendererComponent(const SpriteRendererComponent&) = default;
